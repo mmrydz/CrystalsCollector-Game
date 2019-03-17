f372c4d4bd1b87ea42d58436a14247b970230002
mmrydz <megrydzewski@gmail.com>
2019-03-17 12:09:47 -0400

    getting there!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,121 +1,129 @@
-var randomnumber = Math.floor(Math.random() * 120) + 19  ;
+$( document ).ready(function() {
+  console.log( "ready!" );
+  
+  // With the format arranged with html and the styling set by css, we then add the dynamic functionality with javascript.
 
-randomnumber = parseInt(randomnumber);
+  // Step 1: We start by generating a random number from 19 to 120 (per instructions)
+  // and log this as the computer's random number (and converting it from string to integer):
 
-console.log(randomnumber);
+  var randomnumber = Math.floor(Math.random() * 120) + 19  ;
 
-var counter = 0;
+  randomnumber = parseInt(randomnumber);
 
-// We begin by expanding our array to include all possible options for all three buttons.
+  console.log(randomnumber);
 
-var gemoptionsstart = 1;
-var gemoptionsend = 12;
-var gemoptions = [];
+  // We return this number to the webpage in the randomnumberbox:
 
+  $("#randomnumber").text(randomnumber);
 
-// We identify all possible options for the buttons and put them in an array named gemoptions:
-for (var i = gemoptionsstart; i <gemoptionsend + 1; i++) {
-    gemoptions.push(i);
-}
+  // Step 2: We set up an array of possible values for the four gem buttons.
+  var gemupperlimit = 12;
+  var gemlowerlimit = 1;
 
-console.log(gemoptions);
-
-// We randomly select 4 of these possible options from that array using a for loop utilizing 
-// the same method we used on the random number with a push at the end:
-
-var gemvalues = [];
-var gemvalue;
-
-for (gemvalue = 0; step < 4; gemvalue++) {
-  // Runs 5 times, with values of step 0 through 4.
-  console.log(gemvalue);
-}
-
-for (var i = gemoptionsstart; i <gemoptionsend + 1; i++) {
-  gemoptions.push(i);
-}
-
-// new array to push random item in
-let fourgems = []
-do {
-let item = getRandomItem(gemoptions);
-fourgems.push(item);
-
-// update the original array and remove the recently pushed item
-
-fourgems.splice(fourgems.indexOf(item), 1);
-count++;
-} while(count < n);
-
-console.log(gemoptions);
-console.log(fourgems);
-
-var item = items[Math.floor(Math.random()*items.length)];
-
-var randomnumber1 = Math.floor(Math.random() * gemoptionsend) + gemoptionsstart  ;
-randomnumber1 = parseInt(randomnumber1);
-console.log(randomnumber1);
-
-var randomnumber2 = Math.floor(Math.random() * gemoptionsend) + gemoptionsstart  ;
-randomnumber2 = parseInt(randomnumber2);
-console.log(randomnumber2);
-
-var randomnumber3 = Math.floor(Math.random() * gemoptionsend) + gemoptionsstart  ;
-randomnumber3 = parseInt(randomnumber3);
-console.log(randomnumber3);
-
-var randomnumber4 = Math.floor(Math.random() * gemoptionsend) + gemoptionsstart  ;
-randomnumber4 = parseInt(randomnumber4);
-console.log(randomnumber4);
-
-// We assign each of these numbers to an image.
-
-
-
-// Next we create a for loop to create crystals for every numberOption.
-for (var i = 0; i < gemoptions.length; i++) {
-
-  // For each iteration, we will create an imageCrystal
-  var imageCrystal = $("<img>");
-
-  // First each crystal will be given the class ".crystal-image".
-  // This will allow the CSS to take effect.
-  imageCrystal.addClass("crystal-image");
-
-  // Each imageCrystal will be given a src link to the crystal image
-  imageCrystal.attr("src", url(".../images/bluecrystal.jpg") + i + url("./images/greencrystal.jpg") + i + url("./images/redcrystal.jpg") + i + url("./images/bluecrystal.jpg"));
-
-  // Each imageCrystal will be given a data attribute called data-crystalValue.
-  // This data attribute will be set equal to the array value.
-  imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-  // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-  $("#crystals").append(imageCrystal);
-}
-
-// This time, our click event applies to every single crystal on the page. Not just one.
-$(".crystal-image").on("click", function() {
-
-  // Determining the crystal's value requires us to extract the value from the data attribute.
-  // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-  // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-  // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-
-  var crystalValue = ($(this).attr("data-crystalvalue"));
-  crystalValue = parseInt(crystalValue);
-  // We then add the crystalValue to the user's "counter" which is a global variable.
-  // Every click, from every crystal adds to the global counter.
-  counter += crystalValue;
-
-  // All of the same game win-lose logic applies. So the rest remains unchanged.
-  alert("New score: " + counter);
-
-  if (counter === targetNumber) {
-    alert("You win!");
+  var gemvalues = [];
+  for (var i = gemlowerlimit; i <= gemupperlimit; i++) {
+      gemvalues.push(i);
   }
 
-  else if (counter >= targetNumber) {
-    alert("You lose!!");
+  // Shuffle the gemvalues array and pop off the last four of the shuffled array.
+  // OR do a for loop to randomly select a number, delete it from the array,
+  // choose another random number, delete it from the array, and so on.
+  // With either approach, you extract four unique numbers.
+  
+
+  // Step 3: I decided to take the shuffle approach:
+
+  function shuffle(gemvalues) {
+    var ctr = gemvalues.length, temp, index;
+
+    // While there are elements in the array
+    while (ctr > 0) {
+      // Pick a random index
+        index = Math.floor(Math.random() * ctr);
+      // Decrease ctr by 1
+        ctr--;
+      // And swap the last element with it
+        temp = gemvalues[ctr];
+        gemvalues[ctr] = gemvalues[index];
+        gemvalues[index] = temp;
+    }
+    return gemvalues;
   }
 
+  console.log(shuffle(gemvalues));
+  
+  // Step 4: We then slice out four values from the array.  These will be
+  // assigned to gem image buttons. In this case, I decided to slice off the last
+  // four values from the shuffled array:
+  
+  var fourgems = gemvalues.slice(-4);
+
+  console.log(fourgems);
+  
+  // Step 5: We've now established all of the randome numbers we need.  Now we have to
+  // assign our gem image buttons their values. Then we can set up the on-click events.
+
+  // Step 4: We select the first four numbers in the array as the values for our four gems.
+  // We do it this way so that we have four unique numbers with no repeats.
+  // for (var i = 0; i < 4; i++) {
+  //   var imageCrystal = $("<img>");
+  //   imageCrystal.addClass("crystal-image");
+  //   imageCrystal.attr("src", url(".../images/bluecrystal.jpg") + i + url("./images/greencrystal.jpg") + i + url("./images/redcrystal.jpg") + i + url("./images/bluecrystal.jpg"));
+  //   imageCrystal.attr("data-crystalvalue", fourgems[i]);
+  //   $("#crystals").append(imageCrystal);
+  // }
+
+
+
+  // Step 3: We assign each of the gem values to a gem image button:
+
+
+  // Next we create a for loop to create crystals for every value in fourgems:
+    for (var i = 0; i < fourgems.length; i++) {
+
+    // For each iteration, we will create an imageCrystal
+      var imageCrystal = $("<img>");
+
+    // First each crystal will be given the class ".crystal-image".
+    // This will allow the CSS to take effect.
+      imageCrystal.addClass("crystal-image");
+
+    // Each imageCrystal will be given a src link to the crystal image
+      imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+
+    // Each imageCrystal will be given a data attribute called data-crystalValue.
+    // This data attribute will be set equal to the array value.
+      imageCrystal.attr("data-crystalvalue", fourgems[i]);
+
+    // Lastly, each crystal image (with all it classes and attributes) will get added
+    // in the crystalsbox as defined in the html.
+      $("#crystals").append(imageCrystal);
+  }
+
+  // This time, our click event applies to every single crystal on the page. Not just one.
+  $(".crystal-image").on("click", function() {
+
+    // Determining the crystal's value requires us to extract the value from the data attribute.
+    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
+    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
+    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    // We then add the crystalValue to the user's "counter" which is a global variable.
+    // Every click, from every crystal adds to the global counter.
+    counter += crystalValue;
+
+    // All of the same game win-lose logic applies. So the rest remains unchanged.
+    alert("New score: " + counter);
+
+    if (counter === targetNumber) {
+      alert("You win!");
+    }
+
+    else if (counter >= targetNumber) {
+      alert("You lose!!");
+    }
+
+  });
 });
