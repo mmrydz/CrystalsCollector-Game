41e55167603ac0c82bdbf17ebf94879e88c047d7
mmrydz <megrydzewski@gmail.com>
2019-03-17 12:54:54 -0400

    page looks identical - now working on getting other functionality running

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -78,7 +78,7 @@ $( document ).ready(function() {
       imageCrystal.addClass("crystal-image");
 
     // Each imageCrystal will be given a src link to the crystal image
-      imageCrystal.attr("src", '../images/' + i + '.jpg');
+      imageCrystal.attr("src","./assets/images/" + i + ".jpg");
 
     // Each imageCrystal will be given a data attribute called data-crystalValue.
     // This data attribute will be set equal to the array value.
